af51f415954476f44383c5cea19ca90fcfd752ca
Haoui-collab <76251361+Haoui-collab@users.noreply.github.com>
2020-12-20 00:49:49 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,8 +1,43 @@
-
+#Chargement des library
+library(readxl)
 library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(ggplot2)
+library(dplyr) 
+library(timetk)
+#Importation de la base de données
+labase = read_xlsx("BaseCovidSN.xlsx")
+ggplot(labase, aes(Date, `Cas importes`)) + geom_line()
+ggplot(labase, aes(Date, `Cas contact`)) + geom_line()
+ggplot(labase, aes(Date, `Cas communautaires`)) + geom_line()
+#le serveur
+shinyServer(function(input,output){
 
-# Define server logic required to draw a histogram
-Server= function(input, output) {}
+  output$evolution <- renderPlot({
+    ggplot(labase)+geom_line(aes(Date, `taux de postivité`))
+  })
+  
+#Pour afficher la base de donnés 
+  output$base <- renderTable({
+    data.frame(labase)}
+   )
+#Pour afficher les courbes  
+  output$evolution <- renderPlot({
+    ggplot(labase)+geom_line(aes(Date, `Cas positifs`))
+  })
+  output$cas_positif <- renderPlot({
+    ggplot(labase, aes(Date, `Cas positifs`)) + geom_line()
+  })
+  output$cas_gueri <- renderPlot({
+    ggplot(labase, aes(Date, `Cas testes`)) + geom_line()
+  })
+  output$cas_deces <- renderPlot({
+    ggplot(labase, aes(Date, `Deces`)) + geom_line()
+  })
+  
+})
+
 
 
 
